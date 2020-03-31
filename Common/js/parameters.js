2051429003ef872d13159ca2049870c514caeade
elliott garner <elliott.garner@enginegroup.com>
2020-03-31 11:23:29 +0100

    stype is now a thing

--- Common/js/parameters.js
+++ Common/js/parameters.js
@@ -1,5 +1,7 @@
 const getParameterByName = name => {
-    let url = window.location.href;
+    let url = window.location.href
+    url = url.toLowerCase()
+    name = name.toLowerCase(),
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
@@ -8,7 +10,29 @@ const getParameterByName = name => {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-document.querySelectorAll('.resetLink').forEach(el => {
-    newLink = el.getAttribute('href') + getParameterByName('proj')
+document.querySelectorAll('.resetLink, .mailTo').forEach(el => {
+    newLink = el.getAttribute('href') + getParameterByName('projCode')
     el.setAttribute('href', newLink)
 })
+
+// set up what shows and what doesnt based off stype variable
+const hideElements = classToHide => {
+    document.querySelectorAll(classToHide).forEach(el => {
+        el.remove();
+    })
+}
+
+switch(getParameterByName('stype')) {
+    case '1':
+        break;
+    case '2':
+        hideElements('.resetLink')
+        break;
+    case '3':
+        hideElements('.online')
+        hideElements('.resetLink')
+        break;
+    default:
+        hideElements('.resetLink')
+        break;
+}
